8a947532c4c835859591fd970d43b678368273d5
SmithVyne <smithnkereuwem2@gmail.com>
2021-05-10 21:32:11 +0300

    Init

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import '../styles/App.css';
+import Header from './Header';
+import Body from './Body';
+
+function App() {
+  return (
+    <main className="App">
+      <Header />
+      <Body />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header({header}) {
+    return (
+        <div id="header">
+            <span>User</span>
+        </div>
+    )
+}
